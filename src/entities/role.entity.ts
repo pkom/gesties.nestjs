3bd6b624ca678d21a0a4279f73b5d13b72fffbf6
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-03 12:54:47 +0200

    add role entity and module

---
+++ src/entities/role.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, OneToOne, ManyToMany } from 'typeorm';
+import { BaseEntity, User } from '.';
+
+@Entity({ name: 'roles' })
+export class Role extends BaseEntity {
+  @Column({ type: 'varchar', length: 25, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  description: string;
+
+  @ManyToMany(
+    () => User,
+    user => user.roles,
+  )
+  users: User[];
+}
