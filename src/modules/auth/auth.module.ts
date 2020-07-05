4eeefb0c0b94caa7a688ca534572c5cc104372ba
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-05 19:58:13 +0200

    append ldap strategy to auth module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { LdapStrategy } from './ldap.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LdapStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
