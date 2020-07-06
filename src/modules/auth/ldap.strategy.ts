55545ce666e53dcbb14e491da48bc52f327a423b
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-06 12:16:47 +0200

    add ldap user dto and clean user

--- src/modules/auth/ldap.strategy.ts
+++ src/modules/auth/ldap.strategy.ts
@@ -1,15 +1,21 @@
+import { readFileSync } from 'fs';
+
 import * as Strategy from 'passport-ldapauth';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
 
 import { configService } from '../../config/config.service';
+import { LdapUserDto } from './dto/ldapUserDto';
 
 @Injectable()
 export class LdapStrategy extends PassportStrategy(Strategy, 'ldap') {
   constructor() {
     super(
       {
+        usernameField: 'username',
+        passwordField: 'password',
+        handleErrorsAsFailures: true,
         passReqToCallback: true,
         server: {
           url: configService.getLdapConfig().host,
@@ -17,16 +23,32 @@ export class LdapStrategy extends PassportStrategy(Strategy, 'ldap') {
           bindCredentials: configService.getLdapConfig().password,
           searchBase: 'ou=People,dc=instituto,dc=extremadura,dc=es',
           searchFilter: '(uid={{username}})',
-          searchAttributes: ['employeeNumber', 'cn', 'givenName', 'sn', 'uid'],
+          searchAttributes: [
+            'employeeNumber',
+            'cn',
+            'givenName',
+            'sn',
+            'uid',
+            'uidNumber',
+            'gidNumber',
+            'mail',
+          ],
           groupSearchBase: 'ou=Group,dc=instituto,dc=extremadura,dc=es',
           groupSearchAttributes: ['cn'],
           groupSearchFilter:
             '(&(objectClass=groupOfNames)(memberUid={{username}}))',
+          tlsOptions: {
+            ca: readFileSync(configService.getLdapConfig().certificate),
+          },
         },
       },
-      async (req: Request, user: any, done) => {
-        req.user = user;
-        return done(null, user);
+      async (req: Request, user: LdapUserDto, done) => {
+        if (user._groups) {
+          user.groups = user._groups.map(group => group.cn);
+        }
+        const { controls, _groups, ...userReturned } = user;
+        req.user = userReturned;
+        return done(null, userReturned);
       },
     );
   }
