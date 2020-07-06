87a1642601ce8034a8fcdd60f979af21fbcadedd
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-06 12:14:23 +0200

    set correct paths

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, UseGuards, Request, Post } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { LdapAuthGuard } from './ldap-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LdapAuthGuard)
-  @Post('auth/login')
+  @Post('login')
   async login(@Request() req) {
     return req.user;
   }
