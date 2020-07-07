19bf77690f96fc32a38f2aef3646970c9a214702
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-07 14:03:14 +0200

    add jwt strategy to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
-import { LdapStrategy } from './ldap.strategy';
+import { LdapStrategy } from './strategies/ldap.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { configService } from '../../config/config.service';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LdapStrategy],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: configService.getJwtSecret(),
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LdapStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
