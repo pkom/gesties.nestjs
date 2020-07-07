f0d27dbcd00e44a7d1e84b1ec4d72872996f998b
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-07 14:04:20 +0200

    add sample jwt guard to user profile handler

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, UseGuards, Request, Post } from '@nestjs/common';
-import { LdapAuthGuard } from './ldap-auth.guard';
+import { Controller, UseGuards, Request, Post, Get } from '@nestjs/common';
+import { LdapAuthGuard } from './guards/ldap-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(LdapAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
