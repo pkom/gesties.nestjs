a64cb280966fc6d4cb62e15c92dcf680ae08f9eb
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-07 14:09:18 +0200

    add jwt to auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { LdapUserDto } from './dto/ldapUserDto';
+import { User } from '../../entities';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  login(ldapUserDto: LdapUserDto) {
+    const user = new User();
+    user.uid = ldapUserDto.uid;
+    user.cn = ldapUserDto.cn;
+    user.email = ldapUserDto.email;
+    user.employeeNumber = ldapUserDto.employeeNumber;
+    user.gidNumber = ldapUserDto.gidNumber;
+    user.uidNumber = ldapUserDto.uidNumber;
+    user.givenName = ldapUserDto.givenName;
+    user.sn = ldapUserDto.sn;
+    const payload = {
+      username: user.cn,
+      sub: user.uid,
+    };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 }
