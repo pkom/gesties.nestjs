4be9da4417874b4182dad2743c6a2a3c725dce68
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-15 13:34:00 +0200

    refactor ldap strategy

--- src/modules/auth/strategies/ldap.strategy.ts
+++ src/modules/auth/strategies/ldap.strategy.ts
@@ -12,64 +12,59 @@ import { AppConfigService } from '../../../config/config.service';
 @Injectable()
 export class LdapStrategy extends PassportStrategy(Strategy, 'ldap') {
   constructor(private config: AppConfigService) {
-    super(
-      {
-        usernameField: 'username',
-        passwordField: 'password',
-        handleErrorsAsFailures: true,
-        passReqToCallback: true,
-        server: {
-          url: config.ldapHost,
-          bindDN: config.ldapUser,
-          bindCredentials: config.ldapPass,
-          searchBase: 'ou=People,dc=instituto,dc=extremadura,dc=es',
-          searchFilter: '(uid={{username}})',
-          searchAttributes: [
-            'employeeNumber',
-            'cn',
-            'givenName',
-            'sn',
-            'uid',
-            'uidNumber',
-            'gidNumber',
-            'mail',
-          ],
-          groupSearchBase: 'ou=Group,dc=instituto,dc=extremadura,dc=es',
-          groupSearchAttributes: ['cn'],
-          groupSearchFilter:
-            '(&(objectClass=groupOfNames)(memberUid={{username}}))',
-          tlsOptions: {
-            ca: readFileSync(config.ldapCert),
-          },
+    super({
+      passReqToCallback: true,
+      server: {
+        url: config.ldapHost,
+        bindDN: config.ldapUser,
+        bindCredentials: config.ldapPass,
+        searchBase: 'ou=People,dc=instituto,dc=extremadura,dc=es',
+        searchFilter: '(uid={{username}})',
+        searchAttributes: [
+          'employeeNumber',
+          'cn',
+          'givenName',
+          'sn',
+          'uid',
+          'uidNumber',
+          'gidNumber',
+          'mail',
+        ],
+        groupSearchBase: 'ou=Group,dc=instituto,dc=extremadura,dc=es',
+        groupSearchAttributes: ['cn'],
+        groupSearchFilter:
+          '(&(objectClass=groupOfNames)(memberUid={{username}}))',
+        tlsOptions: {
+          ca: readFileSync(config.ldapCert),
         },
       },
-      async (req: Request, ldapUserDto: LdapUserDto, done) => {
-        if (ldapUserDto._groups) {
-          ldapUserDto.groups = ldapUserDto._groups.map(group => group.cn);
-        }
-        if (ldapUserDto.groups.includes('students')) {
-          return done(
-            new UnauthorizedException('Login not allowed to students'),
-            false,
-          );
-        }
-        delete ldapUserDto.dn;
-        delete ldapUserDto.controls;
-        delete ldapUserDto._groups;
-        const userDto: UserDto = {
-          userName: ldapUserDto.uid,
-          uidNumber: ldapUserDto.uidNumber,
-          gidNumber: ldapUserDto.gidNumber,
-          employeeNumber: ldapUserDto.employeeNumber,
-          firstName: ldapUserDto.givenName,
-          lastName: ldapUserDto.sn,
-          email: ldapUserDto.mail,
-          fullName: ldapUserDto.cn,
-          groups: ldapUserDto.groups,
-        };
-        req.user = userDto;
-        return done(null, userDto);
-      },
-    );
+    });
+  }
+  async validate(req: Request, ldapUserDto: LdapUserDto, done: Function) {
+    if (ldapUserDto._groups) {
+      ldapUserDto.groups = ldapUserDto._groups.map(group => group.cn);
+    }
+    if (ldapUserDto.groups.includes('students')) {
+      return done(
+        new UnauthorizedException('Login not allowed to students'),
+        false,
+      );
+    }
+    delete ldapUserDto.dn;
+    delete ldapUserDto.controls;
+    delete ldapUserDto._groups;
+    const userDto: UserDto = {
+      userName: ldapUserDto.uid,
+      uidNumber: ldapUserDto.uidNumber,
+      gidNumber: ldapUserDto.gidNumber,
+      employeeNumber: ldapUserDto.employeeNumber,
+      firstName: ldapUserDto.givenName,
+      lastName: ldapUserDto.sn,
+      email: ldapUserDto.mail,
+      fullName: ldapUserDto.cn,
+      groups: ldapUserDto.groups,
+    };
+    req.user = userDto;
+    return done(null, userDto);
   }
 }
