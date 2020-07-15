eeb08b609a474be01b182f030c9a7c180f72e9f0
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-15 13:32:14 +0200

    extends LdapAuthGuard to catch some ldap authentication exceptions

--- src/common/shared/guards/ldap-auth.guard.ts
+++ src/common/shared/guards/ldap-auth.guard.ts
@@ -1,5 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LdapAuthGuard extends AuthGuard('ldap') {}
+export class LdapAuthGuard extends AuthGuard('ldap') {
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info, context, status) {
+    // You can throw an exception based on either "info" or "err" arguments
+    if (!err && !user) {
+      if (status === 401) {
+        throw new UnauthorizedException(info.message);
+      } else {
+        throw new BadRequestException(info.message);
+      }
+    }
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
