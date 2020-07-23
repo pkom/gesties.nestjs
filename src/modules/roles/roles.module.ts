e1b91e470579adb4ed944cad12a5dbab70524708
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-23 12:56:37 +0200

    create RolesRepository in roles module

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { Role } from '../../entities';
+
 import { RolesService } from './roles.service';
+import { RolesRepository } from './roles.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [TypeOrmModule.forFeature([RolesRepository])],
   providers: [RolesService],
-  exports: [RolesService],
+  exports: [TypeOrmModule],
 })
 export class RolesModule {}
