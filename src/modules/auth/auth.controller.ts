3ecd7e821a4ecf81998e7e176cb0feaec52bdfac
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-29 12:10:36 +0200

    add user decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, UseGuards, Request, Post, Get } from '@nestjs/common';
 import { LdapAuthGuard } from '../../common/shared/guards/ldap-auth.guard';
 import { JwtAuthGuard } from '../../common/shared/guards/jwt-auth.guard';
 import { AuthService } from './auth.service';
+import { User } from '../../common/shared/decorators/user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +16,7 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  getProfile(@User() user) {
+    return user;
   }
 }
