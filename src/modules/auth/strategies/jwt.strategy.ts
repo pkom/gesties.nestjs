40b99e7daaca8c83f60e3a623793f383736b4b79
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-29 12:52:25 +0200

    add logger to jwt strategy

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AppConfigService } from '../../../config/config.service';
@@ -7,6 +7,8 @@ import { JwtPayload } from '../jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  private logger = new Logger('JwtStrategy');
+
   constructor(
     private readonly config: AppConfigService,
     private readonly authService: AuthService,
@@ -22,11 +24,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     try {
       const user = await this.authService.validateUser(payload);
       if (!user) {
+        this.logger.error(
+          `User validatin user in token ${JSON.stringify(payload)}`,
+        );
         throw new UnauthorizedException();
       }
       // return user;
       done(null, user);
     } catch (error) {
+      this.logger.error(`Error validating token`, error.stack);
       throw new UnauthorizedException('unauthorized', error.message);
     }
   }
