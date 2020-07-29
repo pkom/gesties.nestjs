26896960eeaace889f74e89f0607d03b381a36c1
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-29 13:05:17 +0200

    add logger to auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -13,6 +17,8 @@ import { RolesRepository } from '../roles/roles.repository';
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger('AuthService');
+
   constructor(
     @InjectRepository(UsersRepository)
     private readonly usersRepository: UsersRepository,
@@ -30,6 +36,7 @@ export class AuthService {
       let user = await this.usersRepository.getByName(userDTO.userName);
       if (!user) {
         user = this.usersRepository.create(userDTO);
+        this.logger.debug(`User ${user.userName} has been created`);
       } else {
         await this.usersRepository.update(user.id, { ...userDTO });
       }
@@ -48,10 +55,15 @@ export class AuthService {
         sub: user.id,
         roles: user.roles.map(role => role.name as UserRole),
       };
+      this.logger.verbose(`Token generated for user ${user.userName}`);
       return {
         token: this.jwtService.sign(payload),
       };
     } catch (error) {
+      this.logger.error(
+        `Error generating token for user ${userDTO.userName}`,
+        error.stack,
+      );
       throw new InternalServerErrorException(
         'validateLdapLogin',
         error.message,
@@ -60,40 +72,60 @@ export class AuthService {
   }
 
   private async setRoles(user: User, groups: string[]): Promise<User> {
-    const isAdministrator = groups.includes('administrator');
-    const isAdministration = groups.includes('administration');
-    const isResponsible = groups.includes('responsible');
-    const isTeacher = groups.includes('teachers');
-    const isStudent = groups.includes('students');
-    const roles: Role[] = [];
-    if (isAdministrator) {
-      roles.push(await this.createRole(UserRole.ADMINISTRATOR));
+    try {
+      const isAdministrator = groups.includes('administrator');
+      const isAdministration = groups.includes('administration');
+      const isResponsible = groups.includes('responsible');
+      const isTeacher = groups.includes('teachers');
+      const isStudent = groups.includes('students');
+      const roles: Role[] = [];
+      if (isAdministrator) {
+        roles.push(await this.createRole(UserRole.ADMINISTRATOR));
+      }
+      if (isResponsible) {
+        roles.push(await this.createRole(UserRole.RESPONSIBLE));
+      }
+      if (isAdministration) {
+        roles.push(await this.createRole(UserRole.ADMINISTRATION));
+      }
+      if (isTeacher) {
+        roles.push(await this.createRole(UserRole.TEACHER));
+      }
+      if (isStudent) {
+        roles.push(await this.createRole(UserRole.STUDENT));
+      }
+      user.roles = roles;
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      this.logger.error(
+        `Error setting roles for user ${user.userName}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException(
+        'validateLdapLogin',
+        error.message,
+      );
     }
-    if (isResponsible) {
-      roles.push(await this.createRole(UserRole.RESPONSIBLE));
-    }
-    if (isAdministration) {
-      roles.push(await this.createRole(UserRole.ADMINISTRATION));
-    }
-    if (isTeacher) {
-      roles.push(await this.createRole(UserRole.TEACHER));
-    }
-    if (isStudent) {
-      roles.push(await this.createRole(UserRole.STUDENT));
-    }
-    user.roles = roles;
-    return await this.usersRepository.save(user);
   }
 
   private async createRole(userRole: UserRole): Promise<Role> {
-    let role = await this.rolesRepository.findByRoleName(userRole);
-    if (!role) {
-      const roleDTO = new RoleDTO();
-      roleDTO.name = userRole;
-      roleDTO.description = userRole;
-      role = await this.rolesRepository.save(roleDTO);
+    try {
+      let role = await this.rolesRepository.findByRoleName(userRole);
+      if (!role) {
+        const roleDTO = new RoleDTO();
+        roleDTO.name = userRole;
+        roleDTO.description = userRole;
+        role = await this.rolesRepository.save(roleDTO);
+        this.logger.debug(`Role ${userRole} created`);
+      }
+      return role;
+    } catch (error) {
+      this.logger.error(`Error creating role ${userRole}`, error.stack);
+      throw new InternalServerErrorException(
+        'validateLdapLogin',
+        error.message,
+      );
     }
-    return role;
   }
 
   async validateUser(payload: JwtPayload) {
