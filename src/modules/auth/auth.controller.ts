da2f2ec5b011f109720010103b50b800bc7f9bdc
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-07-29 12:28:12 +0200

    add logger to auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, UseGuards, Request, Post, Get } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Request,
+  Post,
+  Get,
+  Logger,
+} from '@nestjs/common';
 import { LdapAuthGuard } from '../../common/shared/guards/ldap-auth.guard';
 import { JwtAuthGuard } from '../../common/shared/guards/jwt-auth.guard';
 import { AuthService } from './auth.service';
@@ -6,17 +13,20 @@ import { User } from '../../common/shared/decorators/user.decorator';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LdapAuthGuard)
   @Post('login')
   login(@Request() req): Promise<{ token: string }> {
+    this.logger.verbose(`user ${req.user.userName} has logged in`);
     return this.authService.validateLdapLogin(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@User() user) {
+    this.logger.verbose(`user ${user.username} is retrieving his profile`);
     return user;
   }
 }
